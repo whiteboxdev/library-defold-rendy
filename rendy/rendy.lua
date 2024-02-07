a7836b190653dd1d7da725b284c6aa75854d9759
Klayton Kowalski <klaytonkowalski@gmail.com>
2024-02-06 18:01:11 -0600

    Finished screen_to_world and world_to_screen conversions. Maybe 'screen' should be changed to 'viewport' terminology. Progress on demo.

--- rendy/rendy.lua
+++ rendy/rendy.lua
@@ -50,21 +50,28 @@ local function is_within_viewport(camera, screen_x, screen_y)
 		screen_y <= camera.viewport_pixel_y + camera.viewport_pixel_height
 end
 
+local function is_within_ndc_cube(ndc_position)
+	return
+		-1 <= ndc_position.x and ndc_position.x <= 1 and
+		-1 <= ndc_position.y and ndc_position.y <= 1 and
+		-1 <= ndc_position.z and ndc_position.z <= 1
+end
+
 --------------------------------------------------------------------------------
 -- Module Functions
 --------------------------------------------------------------------------------
 
-function rendy.create_camera(id)
-	if rendy.cameras[id] then
-		print("Defold Rendy: rendy.create_camera() -> Camera already exists: " .. id)
+function rendy.create_camera(camera_id)
+	if rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.create_camera() -> Camera already exists: " .. camera_id)
 		return
 	end
-	local camera_url = msg.url(nil, id, "camera")
-	local script_url = msg.url(nil, id, "script")
-	rendy.cameras[id] =
+	local camera_url = msg.url(nil, camera_id, "camera")
+	local script_url = msg.url(nil, camera_id, "script")
+	rendy.cameras[camera_id] =
 	{
-		camera_url = msg.url(nil, id, "camera"),
-		script_url = msg.url(nil, id, "script"),
+		camera_url = msg.url(nil, camera_id, "camera"),
+		script_url = msg.url(nil, camera_id, "script"),
 		viewport_pixel_x = 0,
 		viewport_pixel_y = 0,
 		viewport_pixel_width = 0,
@@ -91,174 +98,167 @@ function rendy.create_camera(id)
 	msg.post(camera_url, message_acquire_camera_focus)
 end
 
-function rendy.destroy_camera(id)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.destroy_camera() -> Camera does not exist: " .. id)
+function rendy.destroy_camera(camera_id)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.destroy_camera() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	msg.post(rendy.cameras[id].camera_url, message_release_camera_focus)
-	rendy.cameras[id] = nil
+	msg.post(rendy.cameras[camera_id].camera_url, message_release_camera_focus)
+	rendy.cameras[camera_id] = nil
 end
 
-function rendy.set_camera_active(id, flag)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.set_camera_active() -> Camera does not exist: " .. id)
+function rendy.set_camera_active(camera_id, flag)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.set_camera_active() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	go.set(rendy.cameras[id].script_url, "active", flag)
-	rendy.cameras[id].active = flag
+	go.set(rendy.cameras[camera_id].script_url, "active", flag)
+	rendy.cameras[camera_id].active = flag
 end
 
-function rendy.set_camera_orthographic(id, flag)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.set_camera_orthographic() -> Camera does not exist: " .. id)
+function rendy.set_camera_orthographic(camera_id, flag)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.set_camera_orthographic() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	go.set(rendy.cameras[id].script_url, "orthographic", flag)
-	rendy.cameras[id].orthographic = flag
+	go.set(rendy.cameras[camera_id].script_url, "orthographic", flag)
+	rendy.cameras[camera_id].orthographic = flag
 end
 
-function rendy.set_camera_resize_mode_center(id)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.set_camera_resize_mode_center() -> Camera does not exist: " .. id)
+function rendy.set_camera_resize_mode_center(camera_id)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.set_camera_resize_mode_center() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	go.set(rendy.cameras[id].script_url, "resize_mode_center", true)
-	go.set(rendy.cameras[id].script_url, "resize_mode_expand", false)
-	go.set(rendy.cameras[id].script_url, "resize_mode_stretch", false)
-	rendy.cameras[id].resize_mode_center = true
-	rendy.cameras[id].resize_mode_expand = false
-	rendy.cameras[id].resize_mode_stretch = false
+	go.set(rendy.cameras[camera_id].script_url, "resize_mode_center", true)
+	go.set(rendy.cameras[camera_id].script_url, "resize_mode_expand", false)
+	go.set(rendy.cameras[camera_id].script_url, "resize_mode_stretch", false)
+	rendy.cameras[camera_id].resize_mode_center = true
+	rendy.cameras[camera_id].resize_mode_expand = false
+	rendy.cameras[camera_id].resize_mode_stretch = false
 end
 
-function rendy.set_camera_resize_mode_expand(id)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.set_camera_resize_mode_expand() -> Camera does not exist: " .. id)
+function rendy.set_camera_resize_mode_expand(camera_id)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.set_camera_resize_mode_expand() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	go.set(rendy.cameras[id].script_url, "resize_mode_center", false)
-	go.set(rendy.cameras[id].script_url, "resize_mode_expand", true)
-	go.set(rendy.cameras[id].script_url, "resize_mode_stretch", false)
-	rendy.cameras[id].resize_mode_center = false
-	rendy.cameras[id].resize_mode_expand = true
-	rendy.cameras[id].resize_mode_stretch = false
+	go.set(rendy.cameras[camera_id].script_url, "resize_mode_center", false)
+	go.set(rendy.cameras[camera_id].script_url, "resize_mode_expand", true)
+	go.set(rendy.cameras[camera_id].script_url, "resize_mode_stretch", false)
+	rendy.cameras[camera_id].resize_mode_center = false
+	rendy.cameras[camera_id].resize_mode_expand = true
+	rendy.cameras[camera_id].resize_mode_stretch = false
 end
 
-function rendy.set_camera_resize_mode_stretch(id)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.set_camera_resize_mode_stretch() -> Camera does not exist: " .. id)
+function rendy.set_camera_resize_mode_stretch(camera_id)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.set_camera_resize_mode_stretch() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	go.set(rendy.cameras[id].script_url, "resize_mode_center", false)
-	go.set(rendy.cameras[id].script_url, "resize_mode_expand", false)
-	go.set(rendy.cameras[id].script_url, "resize_mode_stretch", true)
-	rendy.cameras[id].resize_mode_center = false
-	rendy.cameras[id].resize_mode_expand = false
-	rendy.cameras[id].resize_mode_stretch = true
+	go.set(rendy.cameras[camera_id].script_url, "resize_mode_center", false)
+	go.set(rendy.cameras[camera_id].script_url, "resize_mode_expand", false)
+	go.set(rendy.cameras[camera_id].script_url, "resize_mode_stretch", true)
+	rendy.cameras[camera_id].resize_mode_center = false
+	rendy.cameras[camera_id].resize_mode_expand = false
+	rendy.cameras[camera_id].resize_mode_stretch = true
 end
 
-function rendy.set_camera_viewport(id, x, y, width, height)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.set_camera_viewport() -> Camera does not exist: " .. id)
+function rendy.set_camera_viewport(camera_id, x, y, width, height)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.set_camera_viewport() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	go.set(rendy.cameras[id].script_url, "viewport_fraction_x", x)
-	go.set(rendy.cameras[id].script_url, "viewport_fraction_y", y)
-	go.set(rendy.cameras[id].script_url, "viewport_fraction_width", width)
-	go.set(rendy.cameras[id].script_url, "viewport_fraction_height", height)
-	rendy.cameras[id].viewport_fraction_x = viewport_fraction_x
-	rendy.cameras[id].viewport_fraction_y = viewport_fraction_y
-	rendy.cameras[id].viewport_fraction_width = viewport_fraction_width
-	rendy.cameras[id].viewport_fraction_height = viewport_fraction_height
+	go.set(rendy.cameras[camera_id].script_url, "viewport_fraction_x", x)
+	go.set(rendy.cameras[camera_id].script_url, "viewport_fraction_y", y)
+	go.set(rendy.cameras[camera_id].script_url, "viewport_fraction_width", width)
+	go.set(rendy.cameras[camera_id].script_url, "viewport_fraction_height", height)
+	rendy.cameras[camera_id].viewport_fraction_x = viewport_fraction_x
+	rendy.cameras[camera_id].viewport_fraction_y = viewport_fraction_y
+	rendy.cameras[camera_id].viewport_fraction_width = viewport_fraction_width
+	rendy.cameras[camera_id].viewport_fraction_height = viewport_fraction_height
 end
 
-function rendy.set_camera_range(id, z_min, z_max)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.set_camera_range() -> Camera does not exist: " .. id)
+function rendy.set_camera_range(camera_id, z_min, z_max)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.set_camera_range() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	go.set(rendy.cameras[id].script_url, "z_min", z_min)
-	go.set(rendy.cameras[id].script_url, "z_max", z_max)
-	rendy.cameras[id].z_min = z_min
-	rendy.cameras[id].z_max = z_max
+	go.set(rendy.cameras[camera_id].script_url, "z_min", z_min)
+	go.set(rendy.cameras[camera_id].script_url, "z_max", z_max)
+	rendy.cameras[camera_id].z_min = z_min
+	rendy.cameras[camera_id].z_max = z_max
 end
 
-function rendy.set_camera_resolution(id, width, height)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.set_camera_resolution() -> Camera does not exist: " .. id)
+function rendy.set_camera_resolution(camera_id, width, height)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.set_camera_resolution() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	go.set(rendy.cameras[id].script_url, "resolution_width", width)
-	go.set(rendy.cameras[id].script_url, "resolution_height", height)
-	rendy.cameras[id].resolution_width = width
-	rendy.cameras[id].resolution_height = height
+	go.set(rendy.cameras[camera_id].script_url, "resolution_width", width)
+	go.set(rendy.cameras[camera_id].script_url, "resolution_height", height)
+	rendy.cameras[camera_id].resolution_width = width
+	rendy.cameras[camera_id].resolution_height = height
 end
 
-function rendy.set_camera_zoom(id, zoom)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.set_camera_zoom() -> Camera does not exist: " .. id)
+function rendy.set_camera_zoom(camera_id, zoom)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.set_camera_zoom() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	go.set(rendy.cameras[id].script_url, "zoom", zoom)
-	rendy.cameras[id].zoom = zoom
+	go.set(rendy.cameras[camera_id].script_url, "zoom", zoom)
+	rendy.cameras[camera_id].zoom = zoom
 end
 
-function rendy.set_camera_field_of_view(id, field_of_view)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.set_camera_field_of_view() -> Camera does not exist: " .. id)
+function rendy.set_camera_field_of_view(camera_id, field_of_view)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.set_camera_field_of_view() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	go.set(rendy.cameras[id].script_url, "field_of_view", field_of_view)
-	rendy.cameras[id].field_of_view = field_of_view
+	go.set(rendy.cameras[camera_id].script_url, "field_of_view", field_of_view)
+	rendy.cameras[camera_id].field_of_view = field_of_view
 end
 
 function rendy.get_camera_ids(screen_x, screen_y)
 	local camera_ids = {}
-	for id, camera in pairs(rendy.cameras) do
+	for camera_id, camera in pairs(rendy.cameras) do
 		if is_within_viewport(camera, screen_x, screen_y) then
-			camera_ids[#camera_ids + 1] = id
+			camera_ids[#camera_ids + 1] = camera_id
 		end
 	end
 	return camera_ids
 end
 
-function rendy.screen_to_world(id, screen_x, screen_y)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.screen_to_world() -> Camera does not exist: " .. id)
+function rendy.screen_to_world(camera_id, screen_x, screen_y)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.screen_to_world() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	if not is_within_viewport(rendy.cameras[id], screen_x, screen_y) then
+	if not is_within_viewport(rendy.cameras[camera_id], screen_x, screen_y) then
 		return
 	end
-	local camera_world_position = go.get_world_position(id)
-	local dx_from_viewport = screen_x - rendy.cameras[id].viewport_pixel_x
-	local dy_from_viewport = screen_y - rendy.cameras[id].viewport_pixel_y
-	local viewport_width_compression = 1 / rendy.cameras[id].viewport_fraction_width
-	local viewport_height_compression = 1 / rendy.cameras[id].viewport_fraction_height
-	local resolution_width_ratio = rendy.cameras[id].resolution_width / rendy.window_width
-	local resolution_height_ratio = rendy.cameras[id].resolution_height / rendy.window_height
-	if rendy.cameras[id].resize_mode_center or rendy.cameras[id].resize_mode_expand then
-		local world_x = (dx_from_viewport - rendy.cameras[id].viewport_pixel_width * 0.5) * viewport_width_compression * rendy.cameras[id].zoom
-		local world_y = (dy_from_viewport - rendy.cameras[id].viewport_pixel_height * 0.5) * viewport_height_compression * rendy.cameras[id].zoom
-		return camera_world_position.x + world_x, camera_world_position.y + world_y
-	elseif rendy.cameras[id].resize_mode_stretch then
-		local world_x = (dx_from_viewport - rendy.cameras[id].viewport_pixel_width * 0.5) * viewport_width_compression * resolution_width_ratio * rendy.cameras[id].zoom
-		local world_y = (dy_from_viewport - rendy.cameras[id].viewport_pixel_height * 0.5) * viewport_height_compression * resolution_height_ratio * rendy.cameras[id].zoom
-		return camera_world_position.x + world_x, camera_world_position.y + world_y
-	end
+	local camera_world_position = go.get_world_position(camera_id)
+	local dx_from_viewport = screen_x - rendy.cameras[camera_id].viewport_pixel_x
+	local dy_from_viewport = screen_y - rendy.cameras[camera_id].viewport_pixel_y
+	local viewport_width_compression = 1 / rendy.cameras[camera_id].viewport_fraction_width
+	local viewport_height_compression = 1 / rendy.cameras[camera_id].viewport_fraction_height
+	local world_x = (dx_from_viewport - rendy.cameras[camera_id].viewport_pixel_width * 0.5) * viewport_width_compression * rendy.cameras[camera_id].zoom + camera_world_position.x
+	local world_y = (dy_from_viewport - rendy.cameras[camera_id].viewport_pixel_height * 0.5) * viewport_height_compression * rendy.cameras[camera_id].zoom + camera_world_position.y
+	return  world_x, world_y
 end
 
-function rendy.world_to_screen(id, world_position)
-	if not rendy.cameras[id] then
-		print("Defold Rendy: rendy.screen_to_world() -> Camera does not exist: " .. id)
+function rendy.world_to_screen(camera_id, world_position)
+	if not rendy.cameras[camera_id] then
+		print("Defold Rendy: rendy.world_to_screen() -> Camera does not exist: " .. camera_id)
 		return
 	end
-	local camera_world_position = go.get_world_position(id)
-	if rendy.cameras[id].resize_mode_center or rendy.cameras[id].resize_mode_expand then
-		return 0, 0
-	elseif rendy.cameras[id].resize_mode_stretch then
-		return 0, 0
+	local ndc_position = rendy.cameras[camera_id].frustum * vmath.vector4(world_position.x, world_position.y, world_position.z, 0)
+	if not is_within_ndc_cube(ndc_position) then
+		return
 	end
+	local screen_x = (ndc_position.x + 1) * rendy.cameras[camera_id].viewport_pixel_width * 0.5
+	local screen_y = (ndc_position.y + 1) * rendy.cameras[camera_id].viewport_pixel_height * 0.5
+	return screen_x, screen_y
 end
 
 return rendy
